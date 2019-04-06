744f084addf838ae0294d74dd27f650452095b7c
siwat jomewattana <s1104300051612@gmail.com>
2019-04-06 17:04:55 +0700

    การส่งและรับค่าใน local storage

--- src/pages/getvalue/getvalue.ts
+++ src/pages/getvalue/getvalue.ts
@@ -8,6 +8,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+import { Storage } from '@ionic/storage';
+
 @IonicPage()
 @Component({
   selector: 'page-getvalue',
@@ -15,7 +17,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GetvaluePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  username:string; //สร้างตัวแปร username string
+  studentID:string; //สร้างตัวแปร studentID string
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage:Storage) {
+
+    this.storage.ready().then( //เรียกใช้งาน storage แล้ว
+      ()=>{
+        this.storage.get('username').then(  //รับค่าข้อมูล storage username แล้วนำมาใส่ในตัวแปร username
+          (valName)=>{  //สร้างพารามิเตอร์ valName มารับแล้วมาส่งค่าให้ username
+            this.username=valName;
+          }
+        );
+      }
+    );
+
+    this.storage.ready().then( //เรียกใช้งาน storage แล้ว
+      ()=>{
+        this.storage.get('studentID').then(  //รับค่าข้อมูล studentID แล้วนำมาใส่ในตัวแปร username
+          (valID)=>{  //สร้างพารามิเตอร์ valID มารับแล้วมาส่งค่าให้ studentID
+            this.studentID=valID;
+          }
+        );
+      }
+    );
+
+
+
   }
 
   ionViewDidLoad() {
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Storage } from '@ionic/storage';
+import { GetvaluePage } from '../getvalue/getvalue';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public storage:Storage) {
+    storage.ready().then( //เรียกใช้งาน storage แล้ว
+      ()=>{
+      //set data to local storage
+      storage.set("username",'Siwat.1');
+      storage.set("studentID",'123456789');
+      }
+    );   
+  }
 
+  gotoGetValue(){
+    this.navCtrl.push(GetvaluePage);
   }
 
 }
